1b8a9174d78e64d5d7a0335385ec0340aebb7b3a
Nirushana <kavinda.n.t@gmail.com>
2023-10-01 13:00:59 +0100

    SignIn form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,18 @@
+import { buttonVariants } from '@/components/ui/Button'
+import { Link } from 'lucide-react'
 import { FC } from 'react'
+import { cn } from '@/lib/utils'
+import SignIn from '@/components/SignIn'
 
-interface pageProps {
-  
-}
 
-const page: FC<pageProps> = ({}) => {
-  return <div>page</div>
+const page: FC = () => {
+  return <div className='absolute inset-0'>
+    <div className='h-full max-w2xl mx-auto flex flex-col items-center justify-center gap-20'>
+      <Link href='/' className={cn(buttonVariants({ variant: 'ghost' }), 'self-start -mt-20')}>Home</Link>
+
+      <SignIn />
+    </div>
+  </div>
 }
 
 export default page
